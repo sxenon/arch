e7727b7de09485161cf8166fc9f76ffd3c3c7cb3
sxenon <517512759@qq.com>
2020-01-04 11:13:28 +0800

    补充些文档

---
+++ lib/src/main/java/com/sxenon/arch/controller/BaseViewModel.java
@@ -0,0 +1,9 @@
+package com.sxenon.arch.controller;
+
+import android.arch.lifecycle.ViewModel;
+
+/**
+ *  这个类本生没有任何意义，只是为了表示一种对View 生命周期相关问题的解决办法
+ */
+public class BaseViewModel extends ViewModel {
+}
